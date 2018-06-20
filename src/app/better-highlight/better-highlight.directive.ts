3b563c7b52ca3534ae350128dd20514572699918
Sarah Grimm <sarahagrimm@gmail.com>
2018-06-20 15:42:54 -0700

    binding to a directives property and passing data in. creates alias for input to match ngClass type of binding

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,32 +1,54 @@
-import { Directive, Renderer2, ElementRef, HostListener, HostBinding } from '@angular/core';
+import { Directive, Renderer2, ElementRef, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
-  selector: '[appBetterHighlight]'
+  selector: '[betterHighlight]'
 })
 export class BetterHighlightDirective {
 
   constructor(private renderer: Renderer2, private elementRef: ElementRef) { }
 
-  ngOnInit() {
-    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
-    // fourth argument is optional. we can set !important in that argument
-  }
-  /* Host binding Example */
-@HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
-//bind to host property using HostBinding 
-//define in string
-//camelCase property name because we are accessing DOM property not CSS
+  // ngOnInit() {
+  //   // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+  //   // fourth argument is optional. we can set !important in that argument
+  // }
+
+  /* Binding to directive property example */
+  @Input() defaultColor: string = 'transparent'; // parent to child is input, child to parent is output
+  @Input('betterHighlight') highlightColor: string = 'blue';
+
+@HostBinding('style.backgroundColor') backgroundColor: string = this.defaultColor;
+
+ ngOnInit() {
+   this.backgroundColor = this.defaultColor;
+ }
 
 
 @HostListener('mouseenter') mouseEnter() {
-  this.backgroundColor = 'blue';
+  this.backgroundColor = this.highlightColor;
 }
 
 @HostListener('mouseleave') mouseOut() {
-  this.backgroundColor = 'transparent';
+  this.backgroundColor = this.defaultColor;
 }
+/* End Binding to directive property example */
 }
 
+  /* Host binding Example */
+// @HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+// //bind to host property using HostBinding 
+// //define in string
+// //camelCase property name because we are accessing DOM property not CSS
+
+
+// @HostListener('mouseenter') mouseEnter() {
+//   this.backgroundColor = 'blue';
+// }
+
+// @HostListener('mouseleave') mouseOut() {
+//   this.backgroundColor = 'transparent';
+// }
+
+/* end HostBinding example */ 
 
 /* host listener example */
 // ngOnInit() {
