3af0aaa71b6bfc3c7ccf129c2f4716d626023b71
Sarah Grimm <sarahagrimm@gmail.com>
2018-06-20 15:07:42 -0700

    uses HostBinding to bind to style.backgroundColor of host element to change styles on an event

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, Renderer2, ElementRef, HostListener } from '@angular/core';
+import { Directive, Renderer2, ElementRef, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -11,15 +11,35 @@ export class BetterHighlightDirective {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
     // fourth argument is optional. we can set !important in that argument
   }
+  /* Host binding Example */
+@HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+//bind to host property using HostBinding 
+//define in string
+//camelCase property name because we are accessing DOM property not CSS
 
-  @HostListener('mouseenter') mouseEnter() { // host listener decorator is added to a method we want to execute on an event
-    // listening for mouseenter event on host
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
-  }
 
-  @HostListener('mouseleave') mouseOut() { // host listener decorator is added to a method we want to execute on an event
-    // listening for mouseenter event on host
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
-  }
-
+@HostListener('mouseenter') mouseEnter() {
+  this.backgroundColor = 'blue';
 }
+
+@HostListener('mouseleave') mouseOut() {
+  this.backgroundColor = 'transparent';
+}
+}
+
+
+/* host listener example */
+// ngOnInit() {
+//   // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+//   // fourth argument is optional. we can set !important in that argument
+// }
+
+// @HostListener('mouseenter') mouseEnter() { // host listener decorator is added to a method we want to execute on an event
+//   // listening for mouseenter event on host
+//   this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+// }
+
+// @HostListener('mouseleave') mouseOut() { // host listener decorator is added to a method we want to execute on an event
+//   // listening for mouseleave event on host
+//   this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
+// }
